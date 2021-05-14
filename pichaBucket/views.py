113639946cfff39176b3f1495d33230bc19e09d8
Eston Kagwima <ekagwima745@gmail.com>
2021-05-15 00:43:00 +0300

    adding templates folder and configuring settings

--- pichaBucket/views.py
+++ pichaBucket/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
 
 def landing (request):
-  return render ()
+  return render (request,'index.html')
