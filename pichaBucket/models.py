c3a733b0b7e79df6dee95f92118d8c250011f10f
Eston Kagwima <ekagwima745@gmail.com>
2021-05-15 13:37:53 +0300

    adding category model and its methods

--- pichaBucket/models.py
+++ pichaBucket/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length =50)
+
+
+  def save_category(self):
+    self.save()
+
+  def delete_category(self, name):
+     self.objects.filter(name=name).delete()
+
+  @classmethod
+  def update_category(cls,name,new_name):
+    update = Category.objects.filter(name=name).update(name=new_name)
+    return update
+
+  def __str__(self):
+        return self.name  
+
+
+    
+
+
+
