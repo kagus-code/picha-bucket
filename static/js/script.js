3b0e4312e99d6b4d48e396476f6076dd11d88fc6
Eston Kagwima <ekagwima745@gmail.com>
2021-05-17 00:48:52 +0300

    adding copy url script

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+
+  function copyURL(value){
+    function handler(event){
+      event.clipboardData.setData('text/plain',value);
+      event.preventDefault();
+      document.removeEventListener('copy',handler, true);
+    }
+    document.addEventListener('copy', handler, true);
+    document.execCommand('copy');
+  }
